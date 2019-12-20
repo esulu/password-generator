c72a950b16752e8d201586da33e7050a0eea5f19
Eren Sulutas <erensulu11@gmail.com>
2019-12-20 09:55:43 -0500

    added js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/**
+ * Generates a random lower case letter
+ * Uses the ASCII codes from 97-122 which are a-z respectively
+ */
+function getRandomLower() {
+
+}
+
+console.log(String.fromCharCode(97));
+console.log(String.fromCharCode(122));
+console.log("hello world");
