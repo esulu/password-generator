34cf059b3c14df56609dc9282179c31fa4bc73c2
Eren Sulutas <erensulu11@gmail.com>
2019-12-20 10:00:33 -0500

    random lower case generator

--- main.js
+++ main.js
@@ -4,9 +4,7 @@
  * Uses the ASCII codes from 97-122 which are a-z respectively
  */
 function getRandomLower() {
-
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(String.fromCharCode(97));
-console.log(String.fromCharCode(122));
-console.log("hello world");
+console.log(getRandomLower());
